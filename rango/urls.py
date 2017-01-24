ce3956af2bc84bfebb40964add7dbfca2a0feb80
Peter Vanik <retepvan@yahoo.co.uk>
2017-01-24 18:53:20 +0000

    End of Chapter 3

--- rango/urls.py
+++ rango/urls.py
@@ -1,7 +1,6 @@
-from django.conf.urls import patterns, url
-
+from django.conf.urls import url
 from rango import views
 
-urlpatterns = patterns('',
-
-	url(r'^$', views.index, name='index'))
+urlpatterns = [
+	url(r'^$', views.index, name='index'),
+]
--- rango/views.py
+++ rango/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 def index(request):
 
-	return HttpResponse("Rango says hello world!")
+	return HttpResponse("Rango says hey there partner!")
