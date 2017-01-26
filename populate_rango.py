2a9d38d718f32ea13b32b3876d84364a93f00493
Peter Vanik <retepvan@yahoo.co.uk>
2017-01-26 18:29:33 +0000

    End of Chapter 5 + Exercises

--- populate_rango.py
+++ populate_rango.py
@@ -35,9 +35,9 @@ def populate():
 		{"title":"Flask",
 		 "url":"http://flask.pocoo.org"} ]
 		 
-	cats = {"Python": {"pages": python_pages},
-			"Django": {"pages": django_pages},
-			"Other Frameworks": {"pages": other_pages} }
+	cats = {"Python": {"pages": python_pages, "views":128, "likes":64},
+			"Django": {"pages": django_pages, "views":64, "likes": 32},
+			"Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16} }
 	
 	# If you want to add more categories or pages,
 	# add them to the dictionaries above.
@@ -49,7 +49,7 @@ def populate():
 	# for more information about how to iterate over a dictionary properly.
 
 	for cat, cat_data in cats.items():
-		c = add_cat (cat)
+		c = add_cat(cat, cat_data["views"], cat_data["likes"])
 		for p in cat_data["pages"]:
 			add_page(c, p["title"], p["url"])
 	
@@ -57,7 +57,21 @@ def populate():
 	for c in Category.objects.all():
 		for p in Page.objects.filter(category = c):
 			print ("- {0} - {1}".format(str(c), str(p)))
-
+			
+	# Ch5 exercise: update pop script so that 
+	# Python category has 128 views, 64 likes
+	# Django has 64 views and 32 likes
+	# Other frameworks category has 32 views and 16 likes
+	
+	#print("Trying to print python category likes")
+	#pythCat = Category.objects.filter("Python")
+	#pythCat.views = 128
+	#pythCat.likes = 64
+	#pythCat.save()
+	#print(pythCat.views + "\n")
+	#print(pythCat.likes)
+	#print ("Finished prinitng Python category likes")
+	
 def add_page(cat, title, url, views = 0):
 	p = Page.objects.get_or_create(category = cat, title = title)[0]
 	p.url = url
@@ -65,12 +79,14 @@ def add_page(cat, title, url, views = 0):
 	p.save()
 	return p
 	
-def add_cat (name):
+def add_cat (name, views, likes):
 	c = Category.objects.get_or_create(name = name)[0]
+	c.views = views
+	c.likes = likes
 	c.save()
 	return c
 
 # Start execution here
 if __name__ == '__main__':
-	print ("Strating Rango population script...")
+	print ("Starting Rango population script...")
 	populate()
